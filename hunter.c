4f3a45e7afba3efa09aa3966a472965ec3decc4c
eagerman <khaled.majzoub.2022@gmail.com>
2016-10-13 12:30:42 +1100

    comments

--- hunter.c
+++ hunter.c
@@ -18,15 +18,32 @@
 - try to hunt down vampires before they mature. loosing 2lifepts better than 13 scorePts on each mature vampire
 - hunter should rest (3 lifePts) as soon as their blood level approach 
 - what happenes after hospital?
+- if u encounter an immature vampire u kill him for free :)
+- if u encounter a trap u destroy it and loose 2 lifePts
 
  the map: https://cgi.cse.unsw.edu.au/~cs1927/16s2/ass/ass2/Pics/map2.png
 
 - tracing dracula:
 	- on round 6 if all hunters rest they will know where dracula was on prev turn (6th loc in trail)
-	- if one hunter meets drac in a city all other hunters can see his location in hins trail
+	- if one hunter meets drac in a city all other hunters can see his location in his trail
+	- but NOTE: even if a Hunter is already at the location Dracula has moved to, no encounter occurs 
+		at this time, as dealing with encounters only happens in the action part of a Hunter's turn
+		so the hunter has to wait for his next turn to confront drac
+		while if u enter a city drac is in you can encounter his traps and vamps and confront him in the current turn
 	- keep an eye on drac if he goes to sea or land from sea, we can then follow him
-	- we might want to read drac's trail on every round
-   */
+	- so we want to read drac's trail on every round
+	- a good example on how drac was defeated:
+		https://cgi.cse.unsw.edu.au/~cs1927ass/16s2.dracula/round04/logs/h-g085-d-g028.log.gz
+   
+
+	Drac will most likely try to get to CD by round 6 inorder to get 10lifePts
+		or if he doesnt have a legal move he will get teleported to CD
+	i 1st thought we keep 2 hunters at GA & KL in which they will 100% confront him on the way in or out
+	and the other 2 hunters can scan the map
+	but i then thought that they should not be just standing because we want to minimise vampires from maturing
+	so i thought i will get these 2 hunters each to circulate 3 cities lead 2 CD, 
+
+	*/
 
 #include <stdlib.h>
 #include <stdio.h>
